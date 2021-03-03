cab18bd9c01f60a7fa27c9b83e980a9d089db45b
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2021-03-03 15:43:53 -0800

    add Dutch for comparison

--- tests/adak_test.R
+++ tests/adak_test.R
@@ -1,4 +1,6 @@
 # Test sunrise sunset to make sure it's robust to the two time zones in AK
+# This isn't really a "test" like normal tests because get.sunrise.sunset() doesn't return a value, it just gives a message. But this script can be used to compare sunrise and sunset in Dutch to sunrise/sunset in Adak. 
+
 
 load("G:/GOA/GOA 2021/Files for boats/sunrise sunset/.RData")
 
@@ -15,3 +17,8 @@ get.sunrise.sunset(
 
 #Sunrise is at 2021-03-02 09:55:23 AKST
 #Sunset is at 2021-03-02 20:56:11 AKST
+
+get.sunrise.sunset(
+  chosen.date = get.todays.date(),
+  latitude = 53.8946, # Dutch
+  longitude = -166.5401)
