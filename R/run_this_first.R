52cd6eb6e04b6ce80290fd5ffca210ee063c99ff
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-06-09 07:36:38 -0700

    add dpeends and fix depend path

--- R/run_this_first.R
+++ R/run_this_first.R
@@ -3,12 +3,15 @@
 
 # ************************************************
 # ** MODIFY THIS PATH TO DESCRIBE THE FOLDER WITH THE TAR.GZ FILES!
-depend_path <- "G:/ALEUTIAN/AI 2022/Files for Boats/sunrise sunset/dependencies"
+depend_path <- "G:/ALEUTIAN/AI 2022/Files for Boats/sunrise sunset/2022/dependencies"
 # ***********************************************
 
-to_install <- c("sp_1.5-0.tar.gz", 
+to_install <- c("cpp11_0.4.2.tar.gz",
+                "generics_0.1.2.tar.gz",
+                "sp_1.5-0.tar.gz", 
                 "lubridate_1.8.0.tar.gz", 
-                "maptools_1.1-4.tar.gz")
+                "maptools_1.1-4.tar.gz",
+                "rstudioapi_0.13.tar.gz")
 
 
 for (i in 1:length(to_install)) {
